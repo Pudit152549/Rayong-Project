10f5b78e6987cca6d505edb2c2326110c1984bef
Pudit <pudit.hengsiri@gmail.com>
2026-02-09 11:06:26 +0700

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './style.css'
-import router from './router'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./style.css";
+import router from "./router";
+import { createPinia } from "pinia";
 import { useUserStore } from "@/stores/user";
 
-const app = createApp(App)
-const pinia = createPinia()
-app.use(pinia)
-app.use(router)
-const userStore = useUserStore();
-await userStore.initAuth();
-app.mount('#app')
+async function bootstrap() {
+  const app = createApp(App);
+
+  const pinia = createPinia();
+  app.use(pinia);
+  app.use(router);
+  const userStore = useUserStore();
+  await userStore.initAuth();
+
+  app.mount("#app");
+}
+
+bootstrap();
